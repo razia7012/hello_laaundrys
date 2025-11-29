2533fd5a1df079a16f3b64948b2f3b3de8c3c038
razia7012 <sline7012@gmail.com>
2025-11-29 20:54:20 +0530

    debug mode changed

--- hello_laundry_apis/settings.py
+++ hello_laundry_apis/settings.py
@@ -26,7 +26,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY", "unsafe-dev-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", "False") == "True"
+# DEBUG = os.environ.get("DEBUG", "False") == "True"
+DEBUG = True
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "*").split(",")
 
